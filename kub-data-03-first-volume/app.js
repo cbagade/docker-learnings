f5a019529d18b1e99fdda71db91b6b036f60485a
cbagade <cbagade@yahoo.com>
2023-03-02 20:08:16 +0530

    vol

---
+++ kub-data-03-first-volume/app.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const fs = require("fs");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const filePath = path.join(__dirname, "story", "text.txt");
+
+app.use(bodyParser.json());
+
+app.get("/story", (req, res) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: "Failed to open file." });
+    }
+    res.status(200).json({ story: data.toString() });
+  });
+});
+
+app.post("/story", (req, res) => {
+  const newText = req.body.text;
+  if (newText.trim().length === 0) {
+    return res.status(422).json({ message: "Text must not be empty!" });
+  }
+  fs.appendFile(filePath, newText + "\n", (err) => {
+    if (err) {
+      return res.status(500).json({ message: "Storing the text failed." });
+    }
+    res.status(201).json({ message: "Text was stored!" });
+  });
+});
+
+// this will cause the container to restart but not pod. Pod will simply restart after container crashes
+// pod always restart on container crash
+app.get("/error", () => {
+  process.exit(1);
+});
+
+app.listen(3000);
